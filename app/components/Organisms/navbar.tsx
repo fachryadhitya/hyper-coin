9979edf83072fddedab4a1bad5a6f5468504ae57
fachryadhitya <fachryadhitya@gmail.com>
2024-11-07 09:35:39 +0700

    chore: cleanup

--- app/components/Organisms/navbar.tsx
+++ app/components/Organisms/navbar.tsx
@@ -15,15 +15,12 @@ export default function Navbar({ navContent }: { navContent: INavbar }) {
   const { title, subLinks } = logoSubLinkedData ?? {};
 
   return (
-    <section>
+    <section className="hidden lg:block">
       <nav className="flex justify-between items-center h-[60px] relative box-border px-4 max-w-[1201.5px] w-full mx-auto">
         <div className="logo_image_wrapper relative w-[114px] height-[21px]">
           <Image src={logoData?.icon || ''} alt={logoData?.logo || ''} width={114} height={21} priority unoptimized />
         </div>
-        <ul
-          className={`nav_links flex gap-[60px] 
-           sidebar transition-left left-full
-            `}>
+        <ul className={`nav_links flex gap-[60px] sidebar transition-left left-full`}>
           <NavLink className="max-md:hidden">
             <Text>{title}</Text>
             <span className="material-symbols-outlined text-white">keyboard_arrow_down</span>
