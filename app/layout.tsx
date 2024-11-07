263c8a6569d305bed0f90329a965c262b72cf5a8
fachryadhitya <fachryadhitya@gmail.com>
2024-11-07 08:43:31 +0700

    feat: add root layout and home page components for Next.js app

---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+import "./globals.css";
+import "material-symbols";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import Text from "./components/Atoms/text";
+import TopNavHero from "./components/Templates/tophero.template";
+import { IFuncLangParams } from "./interface/interface";
+import { locales } from "@/constants/locals";
+
+
+export const metadata: Metadata = {
+  title: "hyperhire - coin siren",
+  description: "SIΞRRACOIN is a cryptocurrency that was designed to accelerate the use and creation of solar energy.",
+};
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "600", "900"]
+});
+
+export async function generateStaticParams() {
+  return locales
+}
+
+export default async function RootLayout({
+  params,
+  children,
+}: IFuncLangParams & Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { lang } = params;
+
+  return (
+    <html lang={lang || "ko-KR"} className={poppins.className}>
+      <body>
+        <main className="bg-slight_light">
+          <header>
+            <TopNavHero lang={lang || "ko-KR"} />
+          </header>
+          {children}
+          <footer className="pt-9 pb-[60px] md:pt-12 md:pb-20 px-4 max-w-[1201.5px] mx-auto">
+            <Text variant={"lightest_dark"} className="text-[13px] font-black">
+              &copy; 2024 Hyperhire
+            </Text>
+          </footer>
+        </main>
+      </body >
+    </html >
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import { IFuncLangParams } from "./interface/interface";
+import CompanyServicesGrid from "./components/Molecules/CompanyServicesGrid";
+import CompanyInfoGrid from "./components/Molecules/CompanyInfoGrid";
+
+export default async function Home({ params }: IFuncLangParams) {
+  const locale = params.lang || 'en-US';
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+  const url = `${baseUrl}/api/content-data?locale=${locale}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  const { main_content } = data ?? {};
+
+  const firstContent = main_content?.[0];
+  const secondContent = main_content?.[1];
+
+  return (
+    <main>
+      <CompanyServicesGrid company_prime_data={firstContent} />
+
+      <CompanyInfoGrid secondContent={secondContent} />
+    </main>
+  );
+}
