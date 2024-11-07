267ee080e6254972afa5b9da0646857f78d84fdb
fachryadhitya <fachryadhitya@gmail.com>
2024-11-07 08:49:12 +0700

    feat: add API routes for footer, hero, and navbar data
    This commit introduces new API routes to fetch localized content for the
    footer, hero section, and navigation bar. Each route uses the
    getDictionary function to retrieve locale-specific data and returns it
    in a structured JSON format. Error handling for unsupported locales is
    also implemented.

---
+++ app/api/hero-data/route.ts
@@ -0,0 +1,29 @@
+import { getDictionary } from '@/app/dictionaries';
+
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const locale = url.searchParams.get('locale') || 'en-US';
+
+  try {
+    const dictionary = await getDictionary(locale);
+    const leftContentData = dictionary.left_content || [];
+    const rightContentData = dictionary.right_content || [];
+    const siteData = {
+      heroContent: leftContentData,
+      heroContent2: rightContentData,
+    };
+
+    return new Response(JSON.stringify(siteData), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (e) {
+    return new Response(JSON.stringify({ error: 'Locale not Supported!' }), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      status: 404,
+    });
+  }
+}
---
+++ app/api/nav-data/route.ts
@@ -0,0 +1,28 @@
+import { getDictionary } from '@/app/dictionaries';
+
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const locale = url.searchParams.get('locale') || 'en-US';
+
+  try {
+    const dictionary = await getDictionary(locale);
+    const navbarData = dictionary.navbar || [];
+
+    const siteData = {
+      navbar: navbarData,
+    };
+
+    return new Response(JSON.stringify(siteData), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (e) {
+    return new Response(JSON.stringify({ error: 'Locale not Supported!' }), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      status: 404,
+    });
+  }
+}
